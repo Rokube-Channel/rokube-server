21f2bc5ef7b84a7dcb3884b5ca853a9ea85d1dca
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-11-28 14:27:21 -0600

    netlifyv9

--- functions/src/routes/pages/home/index.js
+++ functions/src/routes/pages/home/index.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
-const HomeRequest = async (req, res) => {
-    console.log(__dirname)
-    return res.sendFile(path.resolve(__dirname, 'home.html'));
+const HomeRequest = async (req, res) => { 
+    const filePath = path.resolve(__dirname, 'home.html'); 
+    fs.readFile(filePath, 'utf8', (err, data) => { 
+        if (err) { 
+            res.status(500).send('Error al leer el archivo'); return; 
+        } 
+        res.send(data); 
+    }); 
 }
 
 module.exports = HomeRequest;
