17755da3bf53aa9934fa17bde7e6bd383d7e4596
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-11-28 17:07:16 -0600

    test1.6

--- functions/src/routes/pages/home/index.js
+++ functions/src/routes/pages/home/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 const HomeRequest = async (req, res) => {
-    return res.json({ view: path.join(__dirname, "/src/routes/api/home.html") })
-    return res.sendFile(path.join(__dirname, "home.html"))
+    return res.sendFile(path.join(__dirname, "/home.html"))
 }
 
 module.exports = HomeRequest
