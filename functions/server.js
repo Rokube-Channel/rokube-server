77d7f297b42d2b6c2eca79745f970b90bab9141c
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-11-28 13:42:10 -0600

    netlifyv5

--- functions/server.js
+++ functions/server.js
@@ -2,7 +2,8 @@ const serverless = require("serverless-http")
 
 const server = require("./src/app");
 
-module.exports.handler = async () => { 
+module.exports.handler = async (event, context) => { 
     const app = await server(); 
-    return serverless(app); 
+    const handler = serverless(app); 
+    return handler(event, context); 
 };
