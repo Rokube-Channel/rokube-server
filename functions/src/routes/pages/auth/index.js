866c87d64ecd95f6d09b88e917752938be121dfc
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-11-30 15:42:01 -0600

    test auth 3

--- functions/src/routes/pages/auth/index.js
+++ functions/src/routes/pages/auth/index.js
@@ -34,6 +34,7 @@ const OauthRequest = async (req, res) => {
                 "https://www.googleapis.com/auth/youtube",
                 "https://www.googleapis.com/auth/youtube.force-ssl",
                 "https://www.googleapis.com/auth/youtube-paid-content",
+                "https://www.googleapis.com/auth/accounts.reauth",
             ],
             include_granted_scopes: true,
             prompt: 'consent',
