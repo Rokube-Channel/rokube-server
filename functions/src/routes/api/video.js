eca9c76d61bd944a64c649fcceb887aeae69bd2e
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-12-10 17:44:29 -0600

    update video.js v3

--- functions/src/routes/api/video.js
+++ functions/src/routes/api/video.js
@@ -1,97 +1,21 @@
 const youtubei = require('youtubei.js');
-// const axios = require("axios")
-// const cp = require('child_process');
-// const ffmpeg = require('ffmpeg-static');
 
 const { Innertube } = youtubei
 
 const VideoRequest = async (req, res) => {
-    // const { query } = req
-    // const { id = "" } = query
-
-    // const payload = {
-    //   "playbackContext": {
-    //     "contentPlaybackContext": {
-    //       "vis": 0,
-    //       "splay": false,
-    //       "referer": `https://www.youtube.com/watch?v=${id}`,
-    //       "currentUrl": `/watch?v=${id}`,
-    //       "autonavState": "STATE_ON",
-    //       "autoCaptionsDefaultOn": false,
-    //       "html5Preference": "HTML5_PREF_WANTS",
-    //       "lactMilliseconds": "-1"
-    //     }
-    //   },
-    //   "attestationRequest": {
-    //     "omitBotguardData": true
-    //   },
-    //   "racyCheckOk": true,
-    //   "contentCheckOk": true,
-    //   "videoId": `${id}`,
-    //   "context": {
-    //     "client": {
-    //       "hl": "en",
-    //       "gl": "US",
-    //       "remoteHost": "",
-    //       "screenDensityFloat": 1,
-    //       "screenHeightPoints": 1440,
-    //       "screenPixelDensity": 1,
-    //       "screenWidthPoints": 2560,
-    //       "visitorData": "",
-    //       "clientName": "iOS",
-    //       "clientVersion": "19.09.3",
-    //       "osName": "iOS",
-    //       "osVersion": "15.6",
-    //       "platform": "MOBILE",
-    //       "clientFormFactor": "UNKNOWN_FORM_FACTOR",
-    //       "userInterfaceTheme": "USER_INTERFACE_THEME_LIGHT",
-    //       "originalUrl": "https://www.youtube.com",
-    //       "deviceMake": "Apple",
-    //       "deviceModel": "iPhone10,6",
-    //       "utcOffsetMinutes": -240,
-    //       "memoryTotalKbytes": "8000000"
-    //     },
-    //     "user": {
-    //       "enableSafetyMode": false,
-    //       "lockedSafetyMode": false
-    //     },
-    //     "request": {
-    //       "useSsl": true,
-    //       "internalExperimentFlags": []
-    //     }
-    //   }
-    // }
-
-    // const { streamingData, videoDetails } = await axios.post("https://www.youtube.com/youtubei/v1/player?prettyPrint=false&alt=json", payload)
-    //   .then(({ data }) => {
-    //     const { streamingData, videoDetails } = data
-    //     return { streamingData, videoDetails }
-    //   })
-    //   .catch((error) => {
-    //     console.error(error)
-    //     return null
-    //   })
-
-    // const videoUrl = streamingData.adaptiveFormats.find(format => format.mimeType.includes('video') && format.quality == "tiny").url;
-    // const audioUrl = streamingData.adaptiveFormats.find(format => format.mimeType.includes('audio') && format.quality == "tiny").url;
-
-    // return res.json({
-    //   id: videoDetails.videoId ?? 'ID no disponible',
-    //   title: videoDetails.title ?? 'Título no disponible',
-    //   thumbnails: videoDetails.thumbnail?.thumbnails?.pop()?.url ?? 'URL no disponible',
-    //   author_id: videoDetails.channelId ?? 'ID de autor no disponible',
-    //   author_name: videoDetails.author ?? 'Nombre de autor no disponible',
-    //   video_url: videoUrl ?? 'Video no disponible',
-    //   audio_url: audioUrl ?? "Audio no disponible"
-    // })
-
     const { myOauth: credentials, query } = req
     const { id = "" } = query
 
     let expired = false
 
     try {
-        const innertube = await Innertube.create({ timezone: "GMT-06:00" });
+        const innertube = await Innertube.create();
+
+        const videoData = await innertube.actions.execute('/player', {
+            videoId: id,
+            client: 'iOS',
+            parse: true,
+        });
 
         if (credentials) {
             const timeout = setTimeout(() => {
@@ -107,23 +31,15 @@ const VideoRequest = async (req, res) => {
                 });
 
             clearTimeout(timeout)
+
+            try{
+                const info = await innertube.getInfo(id).catch(err => console.error(err))
+                await info.addToWatchHistory()
+            }
+            catch(err) { 
+                console.error('No se pudo agregar al historial de reproducción:', err.message);
+            }
         }
-
-        const videoData = await innertube.actions.execute('/player', {
-            videoId: id,
-            client: 'iOS',
-            parse: true,
-        });
-
-        //   try{
-        //       const info = await innertube.getInfo(id).catch(err => console.error(err))
-        //       await info.addToWatchHistory()
-        //   }
-        //   catch(err) { 
-        //       console.error('No se pudo agregar al historial de reproducción:', err.message);
-        //   }
-
-        return res.json({ videoData })
 
         if (!expired) {
             return res.json({
