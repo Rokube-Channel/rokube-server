d181cd281f236bf055b27ec1baea567233fa1769
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2024-12-11 14:06:25 -0600

    video.js remove getString
    Ahora solo almacena en historial, el video se solicita desde rokube

--- functions/src/routes/api/video.js
+++ functions/src/routes/api/video.js
@@ -7,8 +7,6 @@ const VideoRequest = async (req, res) => {
     const { id = "" } = query
 
     let expired = false
-
-    console.log(id)
     
     try {
         const innertube = await Innertube.create();
@@ -28,43 +26,17 @@ const VideoRequest = async (req, res) => {
 
             clearTimeout(timeout)
 
-            // try{
-            //     const info = await innertube.getInfo(id).catch(err => console.error(err))
-            //     await info.addToWatchHistory()
-            // }
-            // catch(err) { 
-            //     console.error('No se pudo agregar al historial de reproducción:', err.message);
-            // }
+            try{
+                const info = await innertube.getInfo(id).catch(err => console.error(err))
+                await info.addToWatchHistory()
+            }
+            catch(err) { 
+                console.error('No se pudo agregar al historial de reproducción:', err.message);
+            }
         }
 
-        // const videoData = await innertube.actions.execute('/player', {
-        //     videoId: id,
-        //     client: 'iOS',
-        //     parse: true,
-        // });
-
-        // const videoData = await innertube.actions.execute('/player', {
-        //     videoId: id,
-        //     client: 'iOS',
-        //     parse: true,
-        // });
-
-        const videoData = await innertube.getInfo(id, 'WEB').catch(err => console.error(err))
-
-        return res.json({ videoData })
+        return res.json({ history: "complete" })
         
-        if (!expired) {
-            return res.json({
-                id: videoData.video_details.id || 'ID no disponible',
-                title: videoData.video_details.title || 'Título no disponible',
-                duration: videoData.video_details.duration || 'Duración no disponible',
-                thumbnails: videoData.video_details.thumbnail?.pop()?.url || 'URL no disponible',
-                author_id: videoData.video_details.channel_id || 'ID de autor no disponible',
-                author_name: videoData.video_details.author || 'Nombre de autor no disponible',
-                short_view_count: videoData.video_details.view_count || 'Conteo de vistas no disponible',
-                video_url: videoData.streaming_data.hls_manifest_url || 'Video no disponible',
-            })
-        }
     }
     catch (err) {
         console.error('Error Video:', err);
